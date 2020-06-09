714954592368039b28a73947a7e1c0f85b0e9b2a
hem6 <hem6@users.noreply.github.com>
2020-06-09 23:32:54 +0900

    Display differences in ProgreeBox

--- src/components/ProgressBox.tsx
+++ src/components/ProgressBox.tsx
@@ -6,22 +6,29 @@ type Props = {
   weekCap: number;
 };
 
-const roundTo2decimals = (n: number) => Math.round(n * 100) / 100;
+const daysN = new Date().getDay() + 1;
 
 const ProgressBox: React.FC<Props> = ({ today, todayCap, week, weekCap }) => (
   <div className='box'>
     <h2 className='has-text-weight-bold'>進捗</h2>
-    <h3>本日 - {`${today}kcal / ${todayCap}kcal`}</h3>
+    <h3>
+      本日 - {`${today}kcal / ${todayCap}kcal`} (
+      {today >= todayCap
+        ? `+${today - todayCap}kcal`
+        : `-${todayCap - today}kcal`}
+      )
+    </h3>
     <progress
       className={`progress ${today > todayCap ? 'is-danger' : 'is-info'}`}
       max={todayCap}
       value={today}
     />
     <h3>
-      今週 -{' '}
-      {`${week}kcal / ${weekCap}kcal(${roundTo2decimals(
-        week / (weekCap / 7)
-      )}日分 / ${new Date().getDay() + 1}日目)`}
+      今週 - {`${week}kcal / ${weekCap}kcal`}(標準対比:{' '}
+      {(weekCap / 7) * daysN <= week
+        ? `+${week - (weekCap / 7) * daysN}kcal`
+        : `-${(weekCap / 7) * daysN - week}kcal`}
+      )
     </h3>
     <progress
       className={`progress ${week > weekCap ? 'is-danger' : 'is-info'}`}
